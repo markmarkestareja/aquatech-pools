3e175ec303533e2b0fd09e2778db6e439daba217
Mark Estareja <mark.estareja.me@gmail.com>
2026-03-15 09:45:26 +0800

    Finished why-shoose-us-section

--- components/hero/Offers.tsx
+++ components/hero/Offers.tsx
@@ -28,11 +28,11 @@ export default function Offers(){
                 <p>Lorem ipsum dolor sit amet consectetur. Adipiscing aliquam lorem commodo vitae diam bibendum volutpat eu. Neque dolor nunc quis consequat eget ultrices lacus magna. In eros porta et id maecenas ornare. Nec diam at ullamcorper id vitae morbi tristique semper.</p>
             </div>
 
-            <div className="flex flex-col md:flex-row gap-8">
+            <div className="flex flex-col md:flex-row gap-4">
 
                 {offersCardContents.map((offersCardContent, index) => (
-                    <div key={index} className="flex flex-col gap-6 bg-m-white overflow-hidden rounded-2xl border border-m-grey">
-                        <div className="relative w-full h-[300px] overflow-hidden rounded-2xl">
+                    <div key={index} className="flex flex-col gap-6 bg-m-white overflow-hidden rounded-2xl border border-m-grey h-auto">
+                        <div className="relative w-full h-75 overflow-hidden rounded-2xl">
                             <Image 
                                 src={`/images/${offersCardContent.img}`}
                                 alt={`${offersCardContent.title}`}
@@ -41,7 +41,7 @@ export default function Offers(){
                             />
                         </div>
 
-                        <div className="flex flex-col gap-8 px-4 pb-8">
+                        <div className="flex flex-col justify-between border-1 min-h-75 gap-8 px-4 pb-8">
                             <div className="flex flex-col gap-4">
                                 <h3>{offersCardContent.title}</h3>
                                 <p>{offersCardContent.desc}</p>
--- components/hero/WhyChooseUs.tsx
+++ components/hero/WhyChooseUs.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function WhyChooseUs(){
     return (
-        <section>
-            <div>
+        <section className="flex flex-col lg:flex-row gap-12 h-auto lg:h-lvh">
+            <div className="flex-1 flex flex-col gap-4">
                 <h2>Why Choose Us</h2>
-                <p>Lorem ipsum dolor sit amet consectetur. Fusce donec amet dui fames elit massa ornare. Justo arcu mi consequat dolor congue massa turpis arcu quam. Nulla venenatis natoque faucibus tellus lectus parturient at et imperdiet. </p>
+                <p className="pt-2">Lorem ipsum dolor sit amet consectetur. Fusce donec amet dui fames elit massa ornare. Justo arcu mi consequat dolor congue massa turpis arcu quam. Nulla venenatis natoque faucibus tellus lectus parturient at et imperdiet. </p>
                 <p>Orci vitae id bibendum lorem. Adipiscing duis in eros odio risus. Cursus nam at pellentesque sagittis augue augue id phasellus lectus. Sed ornare odio ut duis sed dolor dolor. Turpis fermentum convallis dui diam. Suspendisse mi donec at aliquet arcu vestibulum. </p>
             </div>
-            <div>
+            <div className="flex-1 relative min-h-80 lg:h-full rounded-2xl overflow-hidden">
                 <Image 
                     src="/images/pool-supplies.webp"
                     alt=""
